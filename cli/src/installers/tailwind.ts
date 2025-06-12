f9e7552418281a1bf009b3da3b424e58913eb45c
MrT <turgeon.dev@gmail.com>
2025-06-11 17:33:20 -0700

    u

--- cli/src/installers/tailwind.ts
+++ cli/src/installers/tailwind.ts
@@ -1,3 +1,10 @@
+import path from 'path'
+import fs from 'fs-extra'
+
+// UTILS
+import { PKG_ROOT } from '../consts.js'
+
+// FUNCTIONS
 import { addPackageDependency } from '../utils/addPackageDependency.js'
 
 // TYPES
@@ -9,4 +16,24 @@ export const tailwindInstaller: Installer = ({ project_dir }) => {
     dependencies: ['tailwindcss', '@tailwindcss/vite'],
     devMode: true,
   })
+
+  const templateDir = path.join(PKG_ROOT, 'template')
+
+  const cssSrc = path.join(
+    templateDir,
+    'configs',
+    'tailwind',
+    'tailwind-index.css'
+  )
+  const cssDest = path.join(project_dir, 'src/styles/index.css')
+  fs.copySync(cssSrc, cssDest)
+
+  const tailwindConfigSrc = path.join(
+    templateDir,
+    'configs',
+    'tailwind',
+    'tailwind.config.ts'
+  )
+  const tailwindConfigDest = path.join(project_dir, 'tailwind.config.ts')
+  fs.copySync(tailwindConfigSrc, tailwindConfigDest)
 }
