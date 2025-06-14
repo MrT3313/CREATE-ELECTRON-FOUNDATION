ed9a1983a634356773599d7aaf157d9cd26c106b
MrT <turgeon.dev@gmail.com>
2025-06-14 15:53:35 -0700

    u

--- cli/template/base/electron.d.ts
+++ cli/template/base/electron.d.ts
@@ -3,16 +3,16 @@ import type { APIResource, DBResource } from './types/resource'
 declare global {
   interface Window {
     api: {
+      getAPIResourceById: (id: number) => Promise<APIResource>
+      getAPIResourceList: () => Promise<{ data: APIResource[] }>
       insertAPIResource: (resource: Omit<APIResource, 'id'>) => Promise<APIResource>
       deleteAPIResourceById: (id: number) => Promise<void>
-      getAPIResource: (id: number) => Promise<APIResource>
-      getAPIResourceList: () => Promise<{ data: APIResource[] }>
     }
     db: {
+      getDBResourceById: (id: number) => Promise<DBResource>
+      getDBResourceList: () => Promise<DBResource[]>
       insertDBResource: (resource: Omit<DBResource, 'id'>) => Promise<DBResource>
       deleteDBResourceById: (id: number) => Promise<void>
-      getDBResource: (id: number) => Promise<DBResource>
-      getDBResourceList: () => Promise<DBResource[]>
     }
     env: {
       CUSTOM_ENV_VAR: string
