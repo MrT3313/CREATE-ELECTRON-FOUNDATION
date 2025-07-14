f568a67fef84c541180aaaace874a29953566d58
MrT <turgeon.dev@gmail.com>
2025-07-14 14:31:38 -0400

    updated package.json versions

--- cli/src/installers/dependencyVersionMap.ts
+++ cli/src/installers/dependencyVersionMap.ts
@@ -3,12 +3,12 @@
  * ####################################################################### */
 export const dependencyVersionMap = {
   // TailwindCSS
-  tailwindcss: '~4.1.10',
-  '@tailwindcss/vite': '~4.1.10',
+  tailwindcss: '~4.1.11',
+  '@tailwindcss/vite': '~4.1.11',
 
   // Drizzle
-  'drizzle-orm': '~0.44.2',
-  'drizzle-kit': '~0.31.1',
+  'drizzle-orm': '~0.44.3',
+  'drizzle-kit': '~0.31.4',
 
   // SQLite
   'better-sqlite3': '~11.10.0',
@@ -22,10 +22,10 @@ export const dependencyVersionMap = {
   '@tanstack/react-query': '~5.80.2',
   '@tanstack/react-query-devtools': '~5.80.2',
   // Tanstack > router
-  '@tanstack/react-router': '~1.121.2',
-  '@tanstack/react-router-devtools': '~1.121.8',
+  '@tanstack/react-router': '~1.127.3',
+  '@tanstack/react-router-devtools': '~1.127.3',
   '@tanstack/router-cli': '~1.121.10',
-  '@tanstack/router-plugin': '~1.121.10',
+  '@tanstack/router-plugin': '~1.127.5',
 } as const
 
 export type AvailableDependencies = keyof typeof dependencyVersionMap
