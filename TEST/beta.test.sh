7497474dab3ff43345f7c4266f2a98e8a5661b2a
MrT <turgeon.dev@gmail.com>
2025-07-15 23:33:00 -0400

    alpha.test.sh & beta.test.sh

--- TEST/beta.test.sh
+++ TEST/beta.test.sh
@@ -1,8 +1,10 @@
-# npx create-electron-foundation@beta z-beta-tsr                          --router=tanstack-router  --styles=none       --database=none      --git=false --install_packages=true --ide=cursor
-# npx create-electron-foundation@beta z-beta-tsr-tailwind                 --router=tanstack-router  --styles=tailwind   --database=none      --git=false --install_packages=true --ide=cursor
-# npx create-electron-foundation@beta z-beta-rr                           --router=react-router     --styles=none       --database=none      --git=false --install_packages=true --ide=vscode
-# npx create-electron-foundation@beta z-beta-rr-tailwind                  --router=react-router     --styles=tailwind   --database=none      --git=false --install_packages=true --ide=vscode
-# npx create-electron-foundation@beta z-beta-tsr-tailwind-sqlite-drizzle  --router=tanstack-router  --styles=tailwind   --database=sqlite    --git=false --install_packages=true --ide=cursor
-# npx create-electron-foundation@beta z-beta-rr-tailwind-sqlite-drizzle   --router=react-router     --styles=tailwind   --database=sqlite    --git=false --install_packages=true --ide=cursor
-# npx create-electron-foundation@beta z-beta-tsr-sqlite-drizzle           --router=tanstack-router  --styles=none       --database=sqlite    --git=false --install_packages=true --ide=cursor
-# npx create-electron-foundation@beta z-beta-rr-sqlite-drizzle            --router=react-router     --styles=none       --database=sqlite    --git=false --install_packages=true --ide=cursor
+npx create-electron-foundation@beta beta-tsr                    --router=tanstack-router  --styles=none       --database=none      --git=false --install_packages=true --ide=cursor
+npx create-electron-foundation@beta beta-rr                     --router=react-router     --styles=none       --database=none      --git=false --install_packages=true --ide=vscode
+
+npx create-electron-foundation@beta beta-tsr-tw                 --router=tanstack-router  --styles=tailwind   --database=none      --git=false --install_packages=true --ide=cursor
+npx create-electron-foundation@beta beta-tsr-tw-sqlite-drizzle  --router=tanstack-router  --styles=tailwind   --database=sqlite    --git=false --install_packages=true --ide=cursor
+npx create-electron-foundation@beta beta-tsr-sqlite-drizzle     --router=tanstack-router  --styles=none       --database=sqlite    --git=false --install_packages=true --ide=cursor
+
+npx create-electron-foundation@beta beta-rr-tw                  --router=react-router     --styles=tailwind   --database=none      --git=false --install_packages=true --ide=vscode
+npx create-electron-foundation@beta beta-rr-tw-sqlite-drizzle   --router=react-router     --styles=tailwind   --database=sqlite    --git=false --install_packages=true --ide=cursor
+npx create-electron-foundation@beta beta-rr-sqlite-drizzle      --router=react-router     --styles=none       --database=sqlite    --git=false --install_packages=true --ide=cursor
